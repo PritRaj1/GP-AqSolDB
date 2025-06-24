2de224f190e12ab2983234ced6d413937888f92b
PritRaj1 <prithvi.raj1@outlook.com>
2025-06-24 10:34:06 -0700

    setup files

---
+++ setup/requirements.py
@@ -0,0 +1,19 @@
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+packages = [
+    "numpy",
+    "matplotlib",
+    "pandas",
+]
+
+for package in packages:
+    try:
+        print(f"Installing {package}...")
+        install(package)
+        print(f"{package} installed successfully.")
+    except subprocess.CalledProcessError:
+        print(f"Failed to install {package}.")
