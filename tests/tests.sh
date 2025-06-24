4f69bea7da6f97bbb410af9e831f96b51d49631e
Prithvi Raj <77790119+PritRaj1@users.noreply.github.com>
2025-06-24 14:04:35 -0700

    log files

---
+++ tests/tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+conda activate GP_sol
+
+> test.log
+
+test_files=$(ls "tests/*.py")
+
+for test_file in $test_files; do
+    echo "Running $test_file..." | tee -a test.log
+    python $test_file 2>&1 | tee -a test.log
+    echo "----------------------------------------" | tee -a test.log
+done
+
+echo "All tests completed."
