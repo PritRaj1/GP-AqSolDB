8cdad1f8dc6545c11a7f1efa6bef7ff21d2fe443
PritRaj1 <prithvi.raj1@outlook.com>
2025-07-25 19:38:02 +0100

    ran new opt

--- main_kan.py
+++ main_kan.py
@@ -365,13 +365,13 @@ def main():
             use_gpu=True,
             max_hidden_layers=4,
             max_hidden_size=30,
-            num_epochs=50,
-            pretrain_iters=20,
+            num_epochs=100,
+            pretrain_iters=50,
             patience=100,
             sampler='tpe',
             available_acts=["NormaliseGaussian", "ReshapeGaussian", "ReduceSumGaussian", "None"],
         )
-        tuner.optimize(n_trials=200)
+        tuner.optimize(n_trials=300)
         gp_kan = create_optimized_network(CONFIG_PATH, PARAMS_PATH)
 
     print("Training GP-KAN network...")
