836cc50d5d6da4ccd564209728585c8b48217eb8
PritRaj1 <prithvi@zettalaboratories.com>
2025-08-14 17:47:07 -0700

    jax setup

--- src/core/models/gp_kan/__init__.py
+++ src/core/models/gp_kan/__init__.py
@@ -1,7 +1,7 @@
 from .gp_kan import GP_KAN
 from .dense_layer import DenseGPLayer
 from .invariant_acts import NormaliseGaussian, ReduceSumGaussian, ReshapeGaussian
-from .normal_dist import NormalDist
+from .normal_dist import NormalDist, get_device_config, setup_jax_device
 
 __all__ = [
     "GP_KAN",
@@ -10,4 +10,6 @@ __all__ = [
     "NormaliseGaussian",
     "ReshapeGaussian",
     "ReduceSumGaussian",
+    "get_device_config",
+    "setup_jax_device",
 ]
